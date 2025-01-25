ed602fd03f8d1fe6401b551f43febbd8fd56c0c5
engels74 <141435164+engels74@users.noreply.github.com>
2025-01-25 12:24:55 +0100

    fix: mock data [skip ci]

---
+++ update-digests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Mock script to prevent digest updates
+echo "🟢 Running mock update-digests.sh script"
+echo "ℹ️ This script is a mock and will not make any changes to VERSION.json"
+echo "ℹ️ Exiting with status code 0 (success)"
+exit 0
---
+++ update-versions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Mock script to prevent version updates
+echo "🟢 Running mock update-versions.sh script"
+echo "ℹ️ This script is a mock and will not make any changes to VERSION.json"
+echo "ℹ️ Exiting with status code 0 (success)"
+exit 0
